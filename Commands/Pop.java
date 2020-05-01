a0ba39b826293ceb0a1b98eaa939225aba691d9e
Savanmi <63403251+Savanmi@users.noreply.github.com>
2020-05-01 21:24:31 +0700

    Логирование

--- Commands/Pop.java
+++ Commands/Pop.java
@@ -3,16 +3,19 @@ package Commands;
 import Factory.CalcCommand;
 import Factory.CommandExecuteException;
 import Factory.Context;
-
+import Factory.Main;
 import java.util.EmptyStackException;
 import java.util.List;
+import java.util.logging.Level;
 
 public class Pop implements CalcCommand {
     public void execute(Context context, List<String> myArgs ) throws CommandExecuteException {
         try {
+            Main.LOGGER.log(Level.INFO, "pop from stack");
             context.popFromStack();
         } catch(EmptyStackException e) {
-            throw new CommandExecuteException("Cannot execute command because stack is empty", e);
+            Main.LOGGER.log(Level.INFO, "error" + e);
+            throw new CommandExecuteException("stack is empty", e);
         }
     }
 }
