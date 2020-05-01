b4de115e5a41171105a8ac8641d2f41799852fef
Savanmi <63403251+Savanmi@users.noreply.github.com>
2020-05-01 21:23:54 +0700

    Логирование

--- Commands/Mul.java
+++ Commands/Mul.java
@@ -3,9 +3,10 @@ package Commands;
 import Factory.CalcCommand;
 import Factory.CommandExecuteException;
 import Factory.Context;
-
+import Factory.Main;
 import java.util.EmptyStackException;
 import java.util.List;
+import java.util.logging.Level;
 
 public class Mul implements CalcCommand
 {
@@ -13,11 +14,15 @@ public class Mul implements CalcCommand
     public void execute(Context context, List<String> myArgs) throws CommandExecuteException
     {
         try {
+            Main.LOGGER.log(Level.INFO,"get first number");
             double a = context.popFromStack();
+            Main.LOGGER.log(Level.INFO,"get second number");
             double b = context.popFromStack();
+            Main.LOGGER.log(Level.INFO,"get result");
             context.pushToStack(a * b);
         } catch (EmptyStackException e) {
-            throw new CommandExecuteException("Cannot execute command because stack is empty", e);
+            Main.LOGGER.log(Level.INFO,"error: " + e);
+            throw new CommandExecuteException("stack is empty", e);
         }
     }
 }
