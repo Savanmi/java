203a453d3acb9263729322768132ba34fd38a702
Savanmi <63403251+Savanmi@users.noreply.github.com>
2020-05-01 21:25:22 +0700

    Логирование

--- Commands/Push.java
+++ Commands/Push.java
@@ -3,9 +3,10 @@ package Commands;
 import Factory.CalcCommand;
 import Factory.CommandExecuteException;
 import Factory.Context;
-
+import Factory.Main;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.logging.Level;
 
 public class Push implements CalcCommand
 {
@@ -16,15 +17,15 @@ public class Push implements CalcCommand
         if (isDouble(arg)) {
             context.pushToStack(Double.parseDouble(arg));
         } else {
-            // is a constant
             try {
+                Main.LOGGER.log(Level.INFO,"get value");
                 double constantValue = context.getConstant(arg);
+                Main.LOGGER.log(Level.INFO,"push num to stack");
                 context.pushToStack(constantValue);
             } catch (NoSuchElementException e) {
-                throw new CommandExecuteException("Cannot execute command because constant named " + arg + " is not exist", e);
+                throw new CommandExecuteException(arg + " is not exist", e);
             }
         }
-
     }
 
     private boolean isDouble(String str)
@@ -36,6 +37,4 @@ public class Push implements CalcCommand
             return false;
         }
     }
-
-
 }
