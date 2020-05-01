35d4af191223037e38a1128306c3430354291fc7
Savanmi <63403251+Savanmi@users.noreply.github.com>
2020-05-01 21:26:30 +0700

    Логирование

--- Commands/Sub.java
+++ Commands/Sub.java
@@ -3,9 +3,10 @@ package Commands;
 import Factory.CalcCommand;
 import Factory.CommandExecuteException;
 import Factory.Context;
-
+import Factory.Main;
 import java.util.EmptyStackException;
 import java.util.List;
+import java.util.logging.Level;
 
 public class Sub implements CalcCommand
 {
@@ -13,12 +14,14 @@ public class Sub implements CalcCommand
     public void execute(Context context, List<String> myArgs) throws CommandExecuteException
     {
         try {
+            Main.LOGGER.log(Level.INFO,"get first number");
             double b = context.popFromStack();
+            Main.LOGGER.log(Level.INFO,"get second number");
             double a = context.popFromStack();
-
+            Main.LOGGER.log(Level.INFO,"get result");
             context.pushToStack(a - b);
         } catch (EmptyStackException e) {
-            throw new CommandExecuteException("Cannot execute command because stack is empty ", e);
+            throw new CommandExecuteException("stack is empty ", e);
         }
     }
 }
