46e3ef08fe687efe9ef01bbb96109e15335c54a1
Savanmi <63403251+Savanmi@users.noreply.github.com>
2020-05-01 21:27:14 +0700

    Логирование

--- Commands/Sum.java
+++ Commands/Sum.java
@@ -3,23 +3,24 @@ package Commands;
 import Factory.CalcCommand;
 import Factory.CommandExecuteException;
 import Factory.Context;
-
+import Factory.Main;
 import java.util.EmptyStackException;
 import java.util.List;
+import java.util.logging.Level;
 
-public class Sum implements CalcCommand
-{
-
+public class Sum implements CalcCommand {
     @Override
     public void execute(Context context, List<String> myArgs ) throws CommandExecuteException
     {
         try {
+            Main.LOGGER.log(Level.INFO,"get first number");
             double a = context.popFromStack();
+            Main.LOGGER.log(Level.INFO,"get second number");
             double b = context.popFromStack();
+            Main.LOGGER.log(Level.INFO,"get result");
             context.pushToStack(a+b);
         } catch (EmptyStackException e) {
-            throw new CommandExecuteException("Cannot execute command because stack is empty", e);
+            throw new CommandExecuteException("stack is empty", e);
         }
     }
-
 }
