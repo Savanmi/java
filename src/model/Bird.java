f427c09e46059c370009fdb29f1e9329820f862a
Savanmi <63403251+Savanmi@users.noreply.github.com>
2020-07-01 11:14:46 +0700

    Update Bird.java

--- src/model/Bird.java
+++ src/model/Bird.java
@@ -1,23 +1,22 @@
 package model;
 
-import controller.Controller;
-import view.ImageCache;
-import view.Render;
-import java.awt.Image;
-import java.awt.event.KeyEvent;
 
-public class Bird {
-    public int x;
-    public int y;
-    public int width;
-    public int height;
-    public boolean dead;
-    public double jumpHeight;
-    public double gravity;
+import java.awt.*;
+
+class Bird {
+    int x;
+    int y;
+    int width;
+    int height;
+    boolean dead;
+    private double jumpHeight;
+    private double gravity;
     private Image image;
-    private Controller keyboard;
 
-    public Bird() {
+    private int jumpingStack;
+
+    Bird() {
+        jumpingStack = 0;
         x = 100;
         y = 150;
         jumpHeight = 0;
@@ -25,27 +24,24 @@ public class Bird {
         height = 32;
         gravity = 0.5;
         dead = false;
-        keyboard = Controller.getInstance();
     }
 
-    public void update() {
+
+    void jump(){
+        jumpingStack += 4;
+    }
+
+    void update() {
         jumpHeight += gravity;
 
-        if (!dead && keyboard.pressed(KeyEvent.VK_SPACE) ) {
+        if (!dead && jumpingStack != 0) {
+            jumpingStack--;
             jumpHeight = -10;
         }
         y += (int) jumpHeight;
     }
 
-    public Render getRender() {
-        Render r = new Render();
-        r.x = x;
-        r.y = y;
-
-        if (image == null) {
-            image = ImageCache.loadImage("C:\\Users\\Анастасия\\BIRD\\src\\images\\ПТИЦА.png");
-        }
-        r.image = image;
-        return r;
+    Point getCoordinates() {
+        return new Point(x, y);
     }
 }
